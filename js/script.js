ab6d3554376738a59d3e28b443d6cbeb52f29f6a
theadammason <adam.mason86@gmail.com>
2017-02-14 15:59:38 -0500

    Added function to pull data from form, and add to object nested in a blank array of reservations

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-$()
+$(function(){
+
+
+var reservations=[];
+var inputName;
+var inputEmail;
+
+
+class Reservation {
+  constructor(name, email){
+    this.name = name;
+    this.email = email;
+    // this.seat = seat;
+  }
+}
+$('#inputButton').on('click', function(){
+  inputName = $('#inputName').val();
+  inputEmail = $('#inputEmail').val();
+  var newRes = new Reservation(inputName, inputEmail);
+  reservations.push(newRes);
+  console.log(reservations);
+  console.log(newRes);
+})
+});
