3c355106973f10584500bb3c700e8ad3d2f37870
Eric McCormick <Eric McCormick>
2017-02-14 16:39:46 -0500

    Adding intial toggle, working on logic to push

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ $(function(){
 var reservations=[];
 var inputName;
 var inputEmail;
-
+//Eric test array
+var inputSeat;
 
 class Reservation {
   constructor(name, email){
@@ -16,9 +17,37 @@ class Reservation {
 $('#inputButton').on('click', function(){
   inputName = $('#inputName').val();
   inputEmail = $('#inputEmail').val();
+/*
+  inputSeat = if ($('.seat').hasClass('clicked')){
+    $('.seat').hasClass('clicked').toString;
+  }  )
+*/
   var newRes = new Reservation(inputName, inputEmail);
   reservations.push(newRes);
   console.log(reservations);
   console.log(newRes);
 })
+
+$('.seat').hover(function() {
+  //add opacity
+  $( this ).fadeTo("slow",.25, function(){});
+},
+function(){
+  //remove opacity
+  $( this ).fadeTo("slow",1, function(){});
+});
+
+$('.seat').click('on', function(){
+    $(this).toggleClass('clicked'),
+});
+
+//work on storage of seat
+// now that seats are selected how to funnel them o an array
+//how to put in selector
+
+
+//Div click
+
+
+
 });
