08ded3346b601d4688514f9e9a0791d6ca9a4008
Eric McCormick <Eric McCormick>
2017-02-14 22:35:58 -0500

    Remove and add seats, but not prevent selection

--- js/script.js
+++ js/script.js
@@ -14,31 +14,60 @@ class Reservation {
     this.seat = seat;
   }
 }
+
+$('.seat').click('on', function(){
+  //create a new variable to push to inputSeat
+  var seatNumber = $(this).attr('id');
+  //change color
+    $(this).toggleClass('clicked');
+    //add if not already in the array
+if(inputSeat.indexOf(seatNumber) === -1){
+  inputSeat.push(seatNumber);
+}
+  //remove if already in the array
+  //grep taken from prior example
+  else if(inputSeat.indexOf(seatNumber) > -1){
+    inputSeat = jQuery.grep(inputSeat, function(a) {
+      return a !== seatNumber;
+  }); };
+  
+//testing to see that it comes out
+console.log(inputSeat);
+});
+
 $('#inputButton').on('click', function(){
   inputName = $('#inputName').val();
   inputEmail = $('#inputEmail').val();
-  inputSeat = $('.clicked').prop('id');
+  inputSeat = inputSeat;
+  //inputSeat = $('.clicked').prop('id');
 
 
   var newRes = new Reservation(inputName, inputEmail, inputSeat);
   reservations.push(newRes);
+//if id is included in inputSeat then add class of taken
   console.log(reservations);
   console.log(newRes);
   console.log(inputSeat);
-})
+
+//Removing it
+inputSeat = [];
+//changing class
+$('.clicked').removeClass('clicked').addClass('taken');
+//Prevent from selection
+
+
+});
 
 $('.seat').hover(function() {
   //add opacity
-  $( this ).fadeTo("slow",.25, function(){});
+  $( this ).fadeTo("fast",.25, function(){});
 },
 function(){
   //remove opacity
-  $( this ).fadeTo("slow",1, function(){});
+  $( this ).fadeTo("fast",1, function(){});
 });
 
-$('.seat').click('on', function(){
-    $(this).toggleClass('clicked');
-});
+
 
 //work on storage of seat
 // now that seats are selected how to funnel them o an array
