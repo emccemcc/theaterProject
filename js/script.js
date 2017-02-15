31409e72d3cd64f0d8c33002730734ce6399c722
theadammason <adam.mason86@gmail.com>
2017-02-15 12:05:47 -0500

    beautified code

--- js/script.js
+++ js/script.js
@@ -1,92 +1,102 @@
 $(function(){
 
-$('[data-toggle="popover"]').popover()
-
-var reservations=[];
-var inputName;
-var inputEmail;
-//Eric test array
-var inputSeat = [];
-
-class Reservation {
-  constructor(name, email,seat){
-    this.name = name;
-    this.email = email;
-    this.seat = seat;
-  }
-}
-
-$('.seat').click('on', function(){
-  //create a new variable to push to inputSeat
-  var seatNumber = $(this).attr('id');
-  //change color
-    $(this).toggleClass('clicked');
-    //add if not already in the array and disable adding if already taken
-if(inputSeat.indexOf(seatNumber) === -1 //&& $(this).attr('class') == 'seat clicked'
-){
-  inputSeat.push(seatNumber);
-}
-  //remove if already in the array
-  //grep taken from prior example
-  else if(inputSeat.indexOf(seatNumber) > -1){
-    inputSeat = jQuery.grep(inputSeat, function(a) {
-      return a !== seatNumber;
-  }); };
-$('#seatName').val(inputSeat);
-//testing to see that it comes out
-//console.log(inputSeat);
-});
-
-$('#inputButton').on('click', function(){
-  inputName = $('#inputName').val();
-  inputEmail = $('#inputEmail').val();
-  inputSeat = inputSeat.toString();
-  //inputSeat = $('.clicked').prop('id');
-
-  var newRes = new Reservation(inputName, inputEmail, inputSeat);
-  reservations.push(newRes);
-//if id is included in inputSeat then add class of taken
-  console.log(reservations);
-  //console.log(newRes);
-  //console.log(inputSeat);
-
-//Clearing values from array and from screen
-inputSeat = [];
-$('#inputName').val('');
-$('#inputEmail').val('');
-$('#seatName').val('');
-//changing class of the taken seats
-$('.clicked').removeClass('clicked').addClass('taken');
-
-});
-
-$('.seat').hover(function() {
-  //add opacity
-  $( this ).fadeTo("fast",.25, function(){});
-},
-function(){
-  //remove opacity
-  $( this ).fadeTo("fast",1, function(){});
-});
+    $('[data-toggle="popover"]').popover({
+      trigger: 'hover'
+    });
 
 
-// Pull value from array of reservations based on reserved seat that is clicked
+    var reservations = [];
+    var inputName;
+    var inputEmail;
+    //Eric test array
+    var inputSeat = [];
 
-$('.seat').on('click', function(){
-  if (this.className.indexOf('taken') > -1){
-  //console.log($(this).attr('id'));
-  for (var i=0; i<reservations.length; i++) {
-    if (reservations[i].seat === ($(this).attr('id'))){
-      var checkedSeat = reservations[i];
-      };
-  };
+    class Reservation {
+        constructor(name, email, seat) {
+            this.name = name;
+            this.email = email;
+            this.seat = seat;
+        }
+    }
 
-  }else{
-    console.log("not taken");
-    };
+    $('.seat').click('on', function() {
+        //create a new variable to push to inputSeat
+        var seatNumber = $(this).attr('id');
+        //change color
+        $(this).toggleClass('clicked');
+        //add if not already in the array and disable adding if already taken
+        if (inputSeat.indexOf(seatNumber) === -1 //&& $(this).attr('class') == 'seat clicked'
+        ) {
+            inputSeat.push(seatNumber);
+        }
+        //remove if already in the array
+        //grep taken from prior example
+        else if (inputSeat.indexOf(seatNumber) > -1) {
+            inputSeat = jQuery.grep(inputSeat, function(a) {
+                return a !== seatNumber;
+            });
+        };
+        $('#seatName').val(inputSeat);
+        //testing to see that it comes out
+        //console.log(inputSeat);
+    });
 
-    console.log(checkedSeat);
-});
+    $('#inputButton').on('click', function() {
+        inputName = $('#inputName').val();
+        inputEmail = $('#inputEmail').val();
+        inputSeat = inputSeat.toString();
+        //inputSeat = $('.clicked').prop('id');
+
+        var newRes = new Reservation(inputName, inputEmail, inputSeat);
+        reservations.push(newRes);
+        //if id is included in inputSeat then add class of taken
+        console.log(reservations);
+        //console.log(newRes);
+        //console.log(inputSeat);
+
+        //Clearing values from array and from screen
+        inputSeat = [];
+        $('#inputName').val('');
+        $('#inputEmail').val('');
+        $('#seatName').val('');
+        //changing class of the taken seats
+        $('.clicked').removeClass('clicked').addClass('taken');
+
+    });
+
+    $('.seat').hover(function() {
+            //add opacity
+            $(this).fadeTo("fast", .25, function() {});
+        },
+        function() {
+            //remove opacity
+            $(this).fadeTo("fast", 1, function() {});
+        });
+
+
+    // Pull value from array of reservations based on reserved seat that is clicked
+
+    $('.seat').on('click', function() {
+        if (this.className.indexOf('taken') > -1) {
+            //console.log($(this).attr('id'));
+            for (var i = 0; i < reservations.length; i++) {
+                if (reservations[i].seat === ($(this).attr('id'))) {
+                    var checkedSeat = reservations[i];
+                    $(this).attr({
+                        'data-toggle': "popover",
+                        'title': "Popover Header",
+                        'data-content': "Some content inside the popover"
+                    });
+
+                };
+            };
+
+        } else {
+            console.log("not taken");
+        };
+
+        console.log(checkedSeat);
+    });
 
 });
 
