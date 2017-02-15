e9e5e65168e31eccbdcf0a70cb0d2c20cf56e5a1
theadammason <adam.mason86@gmail.com>
2017-02-15 14:12:09 -0500

    comments

--- js/script.js
+++ js/script.js
@@ -18,46 +18,47 @@ $('.seat').click('on', function(){
   //change color
     $(this).toggleClass('clicked');
     //add if not already in the array and disable adding if already taken
-if(inputSeat.indexOf(seatNumber) === -1 && $(this).attr('class') == 'seat clicked'){
-  inputSeat.push(seatNumber);
-}
+        if(inputSeat.indexOf(seatNumber) === -1 && $(this).attr('class') == 'seat clicked'){
+          inputSeat.push(seatNumber);
+          }
   //remove if already in the array
   //grep taken from prior example
-  else if(inputSeat.indexOf(seatNumber) > -1){
-    inputSeat = jQuery.grep(inputSeat, function(a) {
-      return a !== seatNumber;
-  }); };
-$('#seatName').val(inputSeat);
+          else if(inputSeat.indexOf(seatNumber) > -1){
+          inputSeat = jQuery.grep(inputSeat, function(a) {
+          return a !== seatNumber;
+          });
+          };
+
+
+          $('#seatName').val(inputSeat);
 //testing to see that it comes out
-console.log(inputSeat);
+          console.log(inputSeat);
+
+
 });
+
+
 $('#inputButton').on('click', function(){
   inputName = $('#inputName').val();
   inputEmail = $('#inputEmail').val();
   inputSeat = inputSeat.toString();
   //inputSeat = $('.clicked').prop('id');
 
+// Find the html element that has an ID equal to the value in inputSeat, and change its popover values to show its object content
+
+
+
 
   var newRes = new Reservation(inputName, inputEmail, inputSeat);
   reservations.push(newRes);
 //if id is included in inputSeat then add class of taken
-  console.log(reservations);
-  console.log(newRes);
-  console.log(inputSeat);
 
-// Pull value from array of reservations based on reserved seat that is clicked
 
-    $('.seat').on('click', function() {
-        if (this.className.indexOf('taken') > -1) {
-            //console.log($(this).attr('id'));
-            for (var i = 0; i < reservations.length; i++) {
-                if (reservations[i].seat === ($(this).attr('id'))) {
-                    var checkedSeat = reservations[i];
-                    $(this).html(
-                      "<span>"+checkedSeat.name+"</span>"
-                    );
+  // console.log(reservations);
+  // console.log(newRes);
+  // console.log(inputSeat);
 
-                    
+
 //Clearing values from array and from screen
 inputSeat = [];
 $('#inputName').val('');
@@ -68,6 +69,20 @@ $('.clicked').removeClass('clicked').addClass('taken');
 
 
 });
+
+// Pull value from array of reservations based on reserved seat that is clicked
+
+    // $('.seat').on('click', function() {
+    //     if (this.className.indexOf('taken') > -1) {
+    //         for (var i = 0; i < reservations.length; i++) {
+    //             if (reservations[i].seat === ($(this).attr('id'))) {
+    //                 var checkedSeat = reservations[i];
+    //                 $(this).html("<span>"+checkedSeat.name+"</span>");
+    //               };
+    //             };
+    //           };
+    //   });
+
 
 $('.seat').hover(function() {
   //add opacity
@@ -81,7 +96,7 @@ function(){
 
 
 //work on storage of seat
-// now that seats are selected how to funnel them o an array
+//now that seats are selected how to funnel them o an array
 //how to put in selector
 
 
