da6662b02001fb295bbb3b8d86ee69b118603086
priyankashaji <priyankashaji@gmail.com>
2017-02-15 14:13:03 -0500

    updates

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(function(){
+
 var reservations=[];
 var inputName;
 var inputEmail;
@@ -64,16 +65,34 @@ function(){
   //remove opacity
   $( this ).fadeTo("fast",1, function(){});
 });
-
-
-
 //work on storage of seat
 // now that seats are selected how to funnel them o an array
 //how to put in selector
-
-
 //Div click
-
-
-
 });
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("inputButton");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
